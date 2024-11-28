a6d2344acd578e3cf31f36bd06392e5efe30104b
denniskofismith <denniskofismith@gmail.com>
2024-11-28 13:35:06 +0000

    Alx Interview Questions

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 
 def pascal_triangle(n):
- 
+
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    def get_row(row_index, memo={}):
+        if row_index in memo:
+            return memo[row_index]
 
-    for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]  # First element is always 1
-        for j in range(1, len(prev_row)):
-            new_row.append(prev_row[j - 1] + prev_row[j])
-        new_row.append(1)  # Last element is always 1
-        triangle.append(new_row)
+        if row_index == 1:
+            memo[row_index] = [1]
+        else:
+            prev_row = get_row(row_index - 1, memo)
+            new_row = [1]
+            for i in range(1, len(prev_row)):
+                new_row.append(prev_row[i - 1] + prev_row[i])
+            new_row.append(1)
+            memo[row_index] = new_row
+        return memo[row_index]
 
-    return triangle
+    return [get_row(i) for i in range(1, n + 1)]
